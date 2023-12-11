962a362926804ce9b68e975c533626346765cbe7
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-11 18:04:12 +0200

    Getting all categories

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,20 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create.task.dto';
-import { TasksService } from './tasks.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {
-  constructor(private tasksService: TasksService) {}
-
-  @Post()
-  async createTask(@Body() createTaskDto: CreateTaskDto) {
-    // You can create a unique ID for each task
-    const id = Date.now().toString();
-
-    // Save the task in the database
-    const task = await this.tasksService.createTask(createTaskDto);
-
-    // Return a success message
-    return task;
-  }
-}
+export class TasksController {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,17 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { JsonDB, Config } from 'node-json-db';
-import { Task } from './task.entity';
 
 @Injectable()
-export class TasksService {
-  private db: JsonDB;
-  constructor() {
-    this.db = new JsonDB(new Config('myDataBase', true, false, '/'));
-  }
-  async createTask(task: Task) {
-    const id = Date.now().toString();
-    const taskToSave = { ...task, id }; // Ensure the task has an ID
-    await this.db.push(`/tasks/${id}`, taskToSave);
-    return taskToSave;
-  }
-}
+export class TasksService {}
