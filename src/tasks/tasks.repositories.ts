0664921f427970ef871fda7d662617f5cd272e2e
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-11 18:22:41 +0200

    Appling  error exception on tasks repositories

--- src/tasks/tasks.repositories.ts
+++ src/tasks/tasks.repositories.ts
@@ -1,4 +1,11 @@
-import { db } from "src/main";
-export class TasksRepository{
-    
+import { InternalServerErrorException } from '@nestjs/common';
+import { db } from 'src/main';
+export class TasksRepository {
+  async getTasks() {
+    try {
+      return await db.getData('/tasks');
+    } catch (error) {
+      throw new InternalServerErrorException('Data not found');
+    }
+  }
 }
