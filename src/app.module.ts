71c13c694ace5d5bdd07205c78897c3beda8b114
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-11 11:18:57 +0200

    Created categories  module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
