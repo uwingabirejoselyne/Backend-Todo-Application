b2734e95564579d9a58a3349e180abcba31ab472
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-12 19:00:59 +0200

    class validator installation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Config, JsonDB } from 'node-json-db';
+import { ValidationPipe } from '@nestjs/common';
 
 export const db = new JsonDB(new Config('myDataBase', true, true, '/'));
 db.push('/categories', []);
@@ -8,6 +9,11 @@ db.push('/tasks', []);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
