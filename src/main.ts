f1604c76655b8681c81b1ed5812c54d2d5cbffcf
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-13 14:47:50 +0200

    configuring swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Config, JsonDB } from 'node-json-db';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -9,6 +10,15 @@ db.push('/tasks', []);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('backend-todo')
+    .setDescription('An API for managing Tasks and categories.')
+    .setVersion('1.0')
+    .addTag('Todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
